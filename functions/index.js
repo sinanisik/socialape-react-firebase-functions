f82944f29fcd14fa1d95b1d763fd938dad4bc52f
Sinan Işık <sinan.isik@ug.bilkent.edu.tr>
2020-05-26 21:18:39 +0300

    add image upload function

--- functions/index.js
+++ functions/index.js
@@ -6,7 +6,7 @@ const app = express();
 const FBAuth = require("./util/fbAuth");
 
 const { getAllScreams, postOneScream } = require("./handlers/screams");
-const { signup, login } = require("./handlers/users");
+const { signup, login, uploadImage } = require("./handlers/users");
 
 // Scream routes
 app.get("/screams", getAllScreams);
@@ -15,5 +15,6 @@ app.post("/scream", FBAuth, postOneScream); // Post one scream
 // Users routes
 app.post("/signup", signup);
 app.post("/login", login);
+app.post("/user/image", FBAuth, uploadImage);
 
 exports.api = functions.region("europe-west1").https.onRequest(app);
--- functions/util/validators.js
+++ functions/util/validators.js
@@ -34,8 +34,8 @@ exports.validateSignupData = (data) => {
 exports.validateLoginData = (data) => {
   let errors = {};
 
-  if (isEmpty(user.email)) errors.email = "Must not be empty";
-  if (isEmpty(user.password)) errors.password = "Must not be empty";
+  if (isEmpty(data.email)) errors.email = "Must not be empty";
+  if (isEmpty(data.password)) errors.password = "Must not be empty";
 
   //   if (Object.keys(errors).length > 0) return res.status(400).json(errors);
 
