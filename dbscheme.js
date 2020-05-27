cbfe661a0376b1f1046d263e5fe8bad7d278d6c9
Sinan Işık <sinan.isik@ug.bilkent.edu.tr>
2020-05-27 12:51:12 +0300

    getting and commenting on post

--- dbscheme.js
+++ dbscheme.js
@@ -1,15 +1,34 @@
 let db = {
+  users: [
+    {
+      userId: "dh23ggj5h32g543j5gf43",
+      email: "user@email.com",
+      handle: "user",
+      createdAt: "2019-03-15T10:59:52.798Z",
+      imageUrl: "image/dsfsdkfghskdfgs/dgfdhfgdh",
+      bio: "Hello, my name is user, nice to meet you",
+      website: "https://user.com",
+      location: "Lonodn, UK",
+    },
+  ],
   screams: [
     {
       userHandle: "user",
-      body: "this is the scream body",
-      createdAt: "2020-05-26T11:40:16.912Z",
+      body: "This is a sample scream",
+      createdAt: "2019-03-15T10:59:52.798Z",
       likeCount: 5,
-      commentCount: 2,
+      commentCount: 3,
+    },
+  ],
+  comments: [
+    {
+      userHandle: "user",
+      screamId: "kdjsfgdksuufhgkdsufky",
+      body: "nice one mate!",
+      createdAt: "2019-03-15T10:59:52.798Z",
     },
   ],
 };
-
 const userDetails = {
   // Redux data
   credentials: {
--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,12 @@ const app = express();
 
 const FBAuth = require("./util/fbAuth");
 
-const { getAllScreams, postOneScream } = require("./handlers/screams");
+const {
+  getAllScreams,
+  postOneScream,
+  getScream,
+  commentOnScream,
+} = require("./handlers/screams");
 const {
   signup,
   login,
@@ -17,12 +22,17 @@ const {
 // Scream routes
 app.get("/screams", getAllScreams);
 app.post("/scream", FBAuth, postOneScream); // Post one scream
-app.post("/user/image", FBAuth, uploadImage);
-app.post("/user", FBAuth, addUserDetails);
-app.get("/user", FBAuth, getAuthenticatedUser);
+app.get("/scream/:screamId", getScream);
+// TODO: delete scream
+// TODO: like a  scream
+// TODO: unlike a scream
+app.post("/scream/:screamId/comment", FBAuth, commentOnScream);
 
 // Users routes
 app.post("/signup", signup);
 app.post("/login", login);
+app.post("/user/image", FBAuth, uploadImage);
+app.post("/user", FBAuth, addUserDetails);
+app.get("/user", FBAuth, getAuthenticatedUser);
 
 exports.api = functions.region("europe-west1").https.onRequest(app);
